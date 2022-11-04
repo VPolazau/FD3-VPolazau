759a80734cb58f78e10d588d78db2da2bd82cf7f
VPolazau <VPolazau@gmail.com>
2022-11-04 18:25:51 +0300

    add btn newItem

--- ishop2/src/components/item-list/item-list.js
+++ ishop2/src/components/item-list/item-list.js
@@ -4,10 +4,11 @@ import Item from '../item'
 
 import './item-list.css'
 
-const ItemList = ({ items, onItemDeleted, onItemSelected }) => {
+const ItemList = ({ items, onItemDeleted, onItemSelected, onAddNewItem}) => {
   const nodes = items.map(item => (
     <Item
       body={item.body}
+      selected={item.selected}
       itemId={item.id}
       key={item.id}
       onItemSelected={onItemSelected}
@@ -18,6 +19,9 @@ const ItemList = ({ items, onItemDeleted, onItemSelected }) => {
     <div className='item-list table'>
       <div className='row'>
         <span className='key'>Online Store</span>
+      </div>
+      <div className='div__btn'>
+        <button className='btn-new' onClick={onAddNewItem}>New Item</button>
       </div>
       <div className='container'>{nodes}</div>
     </div>
