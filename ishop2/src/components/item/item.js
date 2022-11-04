8f649e877af397c6ee0bd49e2485dad5728acb1c
VPolazau <VPolazau@gmail.com>
2022-11-04 18:25:14 +0300

    update item

--- ishop2/src/components/item/item.js
+++ ishop2/src/components/item/item.js
@@ -2,24 +2,15 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 import './item.css'
+import ViewInfo from '../view-info'
 
-const Item = ({
-  body: { title, price, imageUrl, quantity, selected },
-  itemId,
-  onItemDeleted,
-  onItemSelected,
-}) => {
-  Item.propTypes = {
-    body: PropTypes.object.isRequired,
-    title: PropTypes.string,
-    price: PropTypes.number,
-    imageUrl: PropTypes.string,
-    quantity: PropTypes.number,
-  }
+const Item = ({ body, itemId, selected, onItemDeleted, onItemSelected }) => {
 
   let classItem = 'item'
 
   if (selected) classItem += ' selected'
+
+  const viewInfo = <ViewInfo body={body} id={itemId} />
 
   return (
     <div className={classItem} onClick={() => onItemSelected(itemId)}>
@@ -31,18 +22,35 @@ const Item = ({
       >
         <span aria-hidden='true'>×</span>
       </button>
+      {selected ? viewInfo : info(body)}
+
+    </div>
+  )
+}
+
+const info = body => {
+  const { title, imageUrl, price, quantity, discountPercentage } = body
+  Item.propTypes = {
+    title: PropTypes.string,
+    price: PropTypes.number,
+    imageUrl: PropTypes.string,
+    quantity: PropTypes.number,
+    discountPercentage: PropTypes.number,
+  }
+  return (
+    <React.Fragment>
       <div className='imageUrl'>
         <img src={`${imageUrl}`} alt='image item' className='image' />
       </div>
       <div className='price info'>
         <span className='with-discount'>{price}$</span>
-        <span className='without-discount'>{(price * 1.3).toFixed(0)}$</span>
+        <span className='without-discount'>{(price * discountPercentage).toFixed(0)}$</span>
       </div>
       <div className='title info'>{title}</div>
       <div className='quantity info'>
         <div className='stock'>stock: {quantity}</div>
       </div>
-    </div>
+    </React.Fragment>
   )
 }
 
