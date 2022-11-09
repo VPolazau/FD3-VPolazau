8fc2255ac125176f42e1d9c3be6b6cb36b12d964
VPolazau <VPolazau@gmail.com>
2022-11-09 18:42:22 +0300

    rainbow-frame init

--- rainbow-frame/src/components/rainbow-frame/rainbow-frame.js
+++ rainbow-frame/src/components/rainbow-frame/rainbow-frame.js
@@ -3,10 +3,29 @@ import React, { Component } from 'react'
 import './rainbow-frame.css'
 
 export default class RainbowFrame extends Component {
+  state = {
+    colors: this.props.colors,
+  }
 
-  
+  wrapperFn = (colors) => {
+    if(colors.length == 1) {
+      return (
+        <div className='wrapper' key={colors[0]} style={{ border: 'solid 10px ' + colors[0] }}>
+          {this.props.children}
+        </div>
+      )
+    }
+    let color = colors.pop()
+    return (
+      <div className='wrapper' key={color} style={{ border: 'solid 10px ' + color }}>
+        {this.wrapperFn(colors)}
+      </div>
+    )
+  }
+
   render() {
-    console.log(this.props.children)
-    return
+    const { colors } = this.state
+
+    return this.wrapperFn(colors)
   }
 }
