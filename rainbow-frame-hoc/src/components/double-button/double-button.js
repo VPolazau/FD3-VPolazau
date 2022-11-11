8b31f007e3726adccd711509bf71d94d9ea41339
VPolazau <VPolazau@gmail.com>
2022-11-11 15:30:26 +0300

    try to use eventEmitter

--- rainbow-frame-hoc/src/components/double-button/double-button.js
+++ rainbow-frame-hoc/src/components/double-button/double-button.js
@@ -1,13 +1,18 @@
 import React from 'react'
+// import {EventEmitter} from 'events'
 
 import './double-button.css'
 
 const DoubleButton = ({ caption1, caption2, onPressed, children }) => {
+
+  // const ee = new EventEmitter()
+  // ee.addListener(caption1, num => alert(num))
+
   return (
     <div className='doubleButton'>
-      <button className='btn' onClick={()=>onPressed(1)}>{caption1}</button>
+      <button className='btn' onClick={() => onPressed(1)}>{caption1}</button>
       {children}
-      <button className='btn' onClick={()=>onPressed(2)}>{caption2}</button>
+      <button className='btn' onClick={() => onPressed(2)}>{caption2}</button>
     </div>
   )
 }
