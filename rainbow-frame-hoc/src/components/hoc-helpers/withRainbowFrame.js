554e26b8e8b74b68dec07b8e044e0b4e65044123
VPolazau <VPolazau@gmail.com>
2022-11-14 16:29:47 +0300

    reduce cool method

--- rainbow-frame-hoc/src/components/hoc-helpers/withRainbowFrame.js
+++ rainbow-frame-hoc/src/components/hoc-helpers/withRainbowFrame.js
@@ -4,12 +4,13 @@ import './withRainbowFrame.css'
 
 const withRainbowFrame = colors => Wrapped => {
   return props => {
-    let child = <Wrapped {...props} />
-    colors.forEach(color => {
-      child = <div className='wrapper' style={{ border: 'solid 10px ' + color }}>{child}</div>
-    });
-
-    return child
+    return colors.reduce((acc, color) => {
+      return (
+        <div className='wrapper' key={color} style={{ border: 'solid 10px ' + color }}>
+          {acc}
+        </div>
+      )
+    }, <Wrapped {...props} />)
   }
 }
 
