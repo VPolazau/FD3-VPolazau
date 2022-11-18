484ce789e694e75e49e39c9670f0fd863347408c
VPolazau <VPolazau@gmail.com>
2022-11-18 15:32:14 +0300

    update newCode

--- newCod/_eventEmitter/VotesAnswer.js
+++ newCod/_eventEmitter/VotesAnswer.js
@@ -1,6 +1,8 @@
 ﻿import React from 'react';
 import PropTypes from 'prop-types';
 
+///////////////////////////////////////////////////////
+//подключение
 import {voteEvents} from './events';
 
 import './VotesAnswer.css';
