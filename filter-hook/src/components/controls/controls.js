1bc58da356da76876f82198efb20e8fd30c5efa6
VPolazau <VPolazau@gmail.com>
2022-11-23 15:18:21 +0300

    memo

--- filter-hook/src/components/controls/controls.js
+++ filter-hook/src/components/controls/controls.js
@@ -1,28 +1,36 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import { eventBtn } from '../../events'
 
 import './controls.css'
 
-const Controls = () => {
-
+const Controls = ({ onTermChanged }) => {
   const onCheckChanged = () => eventBtn.emit('onCheckChanged', 'check')
 
-  return (
-    <div className='Controls'>
-      <input
-        type='checkbox'
-        className='checkbox Controls__checkbox'
-        onClick={onCheckChanged}
-      />
-      <input
-        type='text'
-        className='input-text Controls__input-text'
-      />
-      <button className='reset'>
-        сброс
-      </button>
-    </div>
-  )
+  const onTextChanged = e => {
+    const term = e.target.value
+    return onTermChanged(term.toLowerCase())
+  }
+
+  const memoizeedControls = useMemo(() => {
+    console.log('render Controls')
+    return (
+      <div className='Controls'>
+        <input
+          type='checkbox'
+          className='checkbox Controls__checkbox'
+          onClick={onCheckChanged}
+        />
+        <input
+          type='text'
+          className='input-text Controls__input-text'
+          onChange={onTextChanged}
+        />
+        <button className='reset'>сброс</button>
+      </div>
+    )
+  }, [])
+
+  return memoizeedControls
 }
 
 export default Controls
--- filter-hook/src/components/list/list.js
+++ filter-hook/src/components/list/list.js
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 
 import './list.css'
 
-const List = () => {
-  return (
-    <div className='List'>
-      <textarea className='output List__output' readOnly />
-    </div>
-  )
+const List = ({ filteredWords }) => {
+  const memoizeedList = useMemo(() => {
+    console.log('render List')
+    const words = filteredWords.join('\n')
+    return (
+      <div className='List'>
+        <textarea className='output List__output' value={words} readOnly />
+      </div>
+    )
+  }, [filteredWords])
+
+  return memoizeedList
 }
 
 export default List
